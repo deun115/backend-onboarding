a4b5394baab9c50d7277a9636e66d72788d0da61
deun115 <diddmstj15@naver.com>
2024-12-26 17:19:36 +0900

    [FEAT] settings.py 설정 및 DB models 작성

---
+++ board/models.py
@@ -0,0 +1,44 @@
+from datetime import datetime
+
+from django.db import models
+
+from account.models import Users
+
+
+class Category(models.Model):
+    id = models.AutoField(primary_key=True)
+    is_opened = models.BooleanField(default=False)
+    depth1 = models.CharField(max_length=20, null=False)
+    depth2 = models.CharField(max_length=20, null=True)
+    depth3 = models.CharField(max_length=20, null=True)
+
+    def __str__(self):
+        return self.id
+
+
+class Board(models.Model):
+    uuid = models.UUIDField(primary_key=True)
+    user_id = models.ForeignKey(Users, on_delete=models.CASCADE)
+    category_id = models.ForeignKey(Category, on_delete=models.SET_DEFAULT, default=1)
+    title = models.CharField(max_length=50)
+    content = models.CharField(max_length=250)
+    is_deleted = models.BooleanField(default=False)
+    created_at = models.DateTimeField(default=datetime.now, null=False)
+    updated_at = models.DateTimeField(null=True)
+    deleted_at = models.DateTimeField(null=True)
+
+    def __str__(self):
+        return self.uuid
+
+
+class Comment(models.Model):
+    uuid = models.UUIDField(primary_key=True)
+    user_id = models.ForeignKey(Users, on_delete=models.CASCADE)
+    board_id = models.ForeignKey(Board, on_delete=models.CASCADE)
+    is_deleted = models.BooleanField(default=False)
+    created_at = models.DateTimeField(default=datetime.now, null=False)
+    updated_at = models.DateTimeField(null=True)
+    deleted_at = models.DateTimeField(null=True)
+
+    def __str__(self):
+        return self.uuid
--- config/urls.py
+++ config/urls.py
@@ -15,8 +15,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
+    # TO-DO: CBV로 수정하기
+    path('/', include('account.urls')),
     path('admin/', admin.site.urls),
+    path('board/', include('board.urls')),
 ]
